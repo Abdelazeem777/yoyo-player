462706d31fa4b26db6e7bb7477a35da03279553d
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2021-02-04 16:29:10 +0200

    solve some bugs

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,26 +17,24 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-        appBar: fullscreen == false
-            ? AppBar(
-                backgroundColor: Colors.blue,
-                title: Image(
-                  image: AssetImage('image/yoyo_logo.png'),
-                  fit: BoxFit.fitHeight,
-                  height: 50,
-                ),
-                centerTitle: true,
-              )
-            : null,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Image(
+            image: AssetImage('image/yoyo_logo.png'),
+            fit: BoxFit.fitHeight,
+            height: 50,
+          ),
+          centerTitle: true,
+        ),
         body: Column(
           children: [
             YoYoPlayer(
               aspectRatio: 16 / 9,
               url:
                   // "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
-                  // "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-                  // "https://player.vimeo.com/external/440218055.m3u8?s=7ec886b4db9c3a52e0e7f5f917ba7287685ef67f&oauth2_token_id=1360367101",
-                  "https://sfux-ext.sfux.info/hls/chapter/105/1588724110/1588724110.m3u8",
+                  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+              // "https://player.vimeo.com/external/440218055.m3u8?s=7ec886b4db9c3a52e0e7f5f917ba7287685ef67f&oauth2_token_id=1360367101",
+              //"https://sfux-ext.sfux.info/hls/chapter/105/1588724110/1588724110.m3u8",
               videoStyle: VideoStyle(),
               videoLoadingStyle: VideoLoadingStyle(
                 loading: Center(
@@ -54,12 +52,12 @@ class _MyAppState extends State<MyApp> {
                   ),
                 ),
               ),
-              onfullscreen: (t) {
-                setState(() {
-                  fullscreen = t;
-                });
-              },
             ),
+            Text('test'),
+            Text('test'),
+            Text('test'),
+            Text('test'),
+            Text('test'),
           ],
         ),
       ),
